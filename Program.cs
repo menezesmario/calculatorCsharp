b39eeba9f9cf3f669400574b7d75c2049fd6c025
menezesmario <mario082@gmail.com>
2021-06-07 09:30:30 -0300

    Version 1.0

--- Program.cs
+++ Program.cs
@@ -6,7 +6,36 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Sum();
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Qual cálculo deseja fazer?");
+            Console.WriteLine("1 - Soma");
+            Console.WriteLine("2 - Subtração");
+            Console.WriteLine("3 - Multiplicação");
+            Console.WriteLine("4 - Divisão");
+            Console.WriteLine("5 - Sair");
+
+            Console.WriteLine("-----------------");
+            Console.WriteLine("Selecione a operação: ");
+            
+            short res = short.Parse(Console.ReadLine());
+
+            switch(res)
+            {
+                case 1: Sum(); break;
+                case 2: Subtraction(); break;
+                case 3: Multiplication(); break;
+                case 4: Division(); break;
+                case 5: System.Environment.Exit(0); break;
+
+                default: Menu(); break;
+            }
+
         }
 
         static void Sum()
@@ -23,7 +52,10 @@ namespace Calculator
 
             float resultado = valor1 + valor2;
             Console.WriteLine($"O resultado da soma é {resultado}");
+            Console.ReadKey();
+            Menu();
         }
+
 
         static void Subtraction()
         {
@@ -39,6 +71,8 @@ namespace Calculator
 
             float resultado = valor1 - valor2;
             Console.WriteLine($"O resultado da subtração é {resultado}");
+            Console.ReadKey();
+            Menu();
         }
 
         static void Multiplication()
@@ -54,6 +88,8 @@ namespace Calculator
             Console.WriteLine("");
             float resultado = valor1 * valor2;
             Console.WriteLine($"O resultado da multiplicação é {resultado}");
+            Console.ReadKey();
+            Menu();
         }
 
         static void Division()
@@ -69,7 +105,10 @@ namespace Calculator
             Console.WriteLine("");
 
             float resultado = valor1 / valor2;
-            Console.WriteLine($"O resultado da multiplicação é {resultado}");
+            Console.WriteLine($"O resultado da divisão é {resultado}");
+            
+            Console.ReadKey();
+            Menu();
         }
     }
 }
